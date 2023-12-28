01dc7cdbfc4ed0377a608a7fa2b6ef4154110044
ontor07 <110522352+ontor07@users.noreply.github.com>
2023-12-28 16:53:42 +0600

    slider image

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -2,7 +2,7 @@
 
 namespace App\DataTables;
 
-use App\Models\Menu;
+use App\Models\Slider;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
@@ -12,7 +12,7 @@ use Yajra\DataTables\Html\Editor\Editor;
 use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
 
-class MenuDataTable extends DataTable
+class SliderDataTable extends DataTable
 {
     /**
      * Build the DataTable class.
@@ -23,35 +23,22 @@ class MenuDataTable extends DataTable
     {
         return (new EloquentDataTable($query))
             ->addColumn('action', function($query){
-                $editBtn = "<a href='".route('admin.menu.edit',$query->id)."' class='btn btn-icon btn-primary' ><i class='far fa-edit'></i></a>";
-               $deleteBtn = "<a href='".route('admin.menu.destroy',$query->id)."' class='btn btn-icon btn-danger ml-2 delete-item' ><i class='fa fa-trash'></i></a>";
-               return $editBtn.$deleteBtn;
+                $editBtn = "<a href='".route('admin.slider.edit',$query->id)."' class='btn btn-icon btn-primary' ><i class='far fa-edit'></i></a>";
+            $deleteBtn = "<a href='".route('admin.slider.destroy',$query->id)."' class='btn btn-icon btn-danger ml-2 delete-item' ><i class='fa fa-trash'></i></a>";
+            return $editBtn.$deleteBtn;
             })
-            ->addColumn('icon', function($query){
-                return $icon = "<i class='".$query->icon ."' ></i> ";
+        
+            ->addColumn('banner', function($query){
+                return $img = "<img width= '100px' src ='".asset($query->image)."' >";
             })
-            ->addColumn('Menu Name', function($query){
-                return $query->menu_name_en;
-            })
-            ->addColumn('status', function($query){
-                $active ='<div class="badge badge-success">Active</div>';
-                $inactive ='<div class="badge badge-danger">Not Active</div>';
-                if($query->status == 1)
-                {
-                    return $active;
-                } 
-                else{
-                    return $inactive;
-                }
-            })
-            ->rawColumns(['icon','action','status'])
+            ->rawColumns(['banner','action'])
             ->setRowId('id');
     }
 
     /**
      * Get the query source of dataTable.
      */
-    public function query(Menu $model): QueryBuilder
+    public function query(Slider $model): QueryBuilder
     {
         return $model->newQuery();
     }
@@ -62,7 +49,7 @@ class MenuDataTable extends DataTable
     public function html(): HtmlBuilder
     {
         return $this->builder()
-                    ->setTableId('menu-table')
+                    ->setTableId('slider-table')
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
@@ -84,14 +71,10 @@ class MenuDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            
             Column::make('id'),
-            // Column::make('Parent'),
-            Column::make('Menu Name'),
-            Column::make('icon'),
-            Column::make('route_name'),
+            Column::make('title'),
+            Column::make('banner'),
             Column::make('order_by'),
-            Column::make('status'),
           
             Column::computed('action')
                   ->exportable(false)
@@ -99,6 +82,8 @@ class MenuDataTable extends DataTable
                   ->width(100)
                   ->addClass('text-center'),
            
+           
+          
         ];
     }
 
@@ -107,6 +92,6 @@ class MenuDataTable extends DataTable
      */
     protected function filename(): string
     {
-        return 'Menu_' . date('YmdHis');
+        return 'Slider_' . date('YmdHis');
     }
 }
--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
 use App\Traits\ImageUploadTrait;
+use App\DataTables\SliderDataTable;
 
 class SliderController extends Controller
 {
@@ -13,9 +14,10 @@ class SliderController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(SliderDataTable $dataTable)
     {
-             return view('admin.slider.index');
+        return $dataTable->render('admin.slider.index');
+        
     }
 
     /**
@@ -94,7 +96,8 @@ class SliderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $slider = Slider::findOrFail($id);
+        return view('admin.slider.edit',compact('slider'));
     }
 
     /**
@@ -102,7 +105,45 @@ class SliderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'image' => ['nullable', 'image', 'max:2000'],
+            'title' => ['required', 'max:200'],
+            'order_by' => ['required'],
+        ]);
+        $data = array(
+
+            'image'=>$request->image,
+            'type'=>$request->type,
+            'title'=>$request->title,
+            'starting_prize'=>$request->starting_prize,   
+            'btn_url'=>$request->btn_url,   
+            'order_by'=>$request->order_by,     
+            'status'=>$request->status,   
+            
+            
+        );
+
+        // Handel file upload 
+
+        $slider = Slider::findOrFail($id);
+
+        $image= $this->updateImage($request, 'image', 'uploads/slider', $slider->image);
+        $data['image'] = empty(!$image) ? $image : $slider->image;
+
+        // Handel file upload 
+
+        $update = Slider::find($id)->update($data);
+
+        if($update)
+        {
+            toastr()->success('Data has been Update successfully!');
+            return redirect(route('admin.slider.index'));
+        }
+        else
+        {
+            toastr()->error('An error has occurred please try again later.');
+            return redirect(route('admin.slider.index'));
+        }
     }
 
     /**
